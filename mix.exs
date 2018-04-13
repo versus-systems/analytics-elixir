8261019d7bb3a12534aef41d5f88407d06c653b7
Desmond Bowe <desmond@crevalle.io>
2018-04-13 14:27:33 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AnalyticsElixir.Mixfile do
   def project do
     [
       app: :segment,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.0",
       deps: deps(),
       description: "analytics_elixir",
